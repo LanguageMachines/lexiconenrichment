ef0fe44b03357d9aa6a3475f7e8825a74d8f405a
Maarten van Gompel <proycon@anaproy.nl>
2020-08-19 18:15:10 +0200

    overwrite in case file exists already

--- install_deps.sh
+++ install_deps.sh
@@ -2,6 +2,6 @@
 wget https://ilps.science.uva.nl/ilps/wp-content/uploads/sites/6/files/compound-splitter-nl.tar.gz || exit 2
 tar -xvzf compound-splitter-nl.tar.gz || exit 2
 cd compound-splitter-nl
-ln -s ../compoundwords.edited.out
-ln -s ../compound_server.conf
+ln -sf ../compoundwords.edited.out
+ln -sf ../compound_server.conf
 cd -
